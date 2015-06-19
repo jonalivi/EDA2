d10e07e4c2a7fcfee4efcb8b60f7bb9c7658fc4d
Nadya Polidorova <moooncancer@gmail.com>
2015-06-19 12:35:34 +0300

    plot2.R plotting implemented and plot2.png generated

--- plot2.R
+++ plot2.R
@@ -35,3 +35,46 @@ data.read <- function(data.dir=file.path(".","data")) {
       message(paste("Reading ", scc.file, "...", collapse=""))
       scc <- readRDS(scc.file)
 }
+
+# prepare the data for plotting -------------------------------------------
+data.read()
+## in order to plot total emissions, group
+## the 'scc' dataset by 'year' and summarize the
+## 'Emissions'
+library(dplyr)
+nei <- group_by(nei, year)
+nei.for.plot <- nei %>%
+      filter(fips == "24510") %>%
+      summarize(total_Emissions=sum(Emissions))
+nei.for.plot$year <- as.numeric(nei.for.plot$year)
+
+# plot the data -----------------------------------------------------------
+
+png(filename="./plot2.png")
+## do not draw axes, draw dots and lines
+plot(x=nei.for.plot$year, 
+     y=nei.for.plot$total_Emissions,
+     main="Total Emissions of PM2.5 in Baltimore",
+     xlab="Years", ylab="Total Emissions of PM2.5 (tons)",
+     type='o', axes=FALSE, pch=19, frame=TRUE)
+x <- nei.for.plot$year
+y <- nei.for.plot$total_Emissions
+## label values near the dots
+sub <- c(F,T,F,T)
+text(x=subset(x,sub),
+     y=subset(y,sub),
+     labels = subset(format(y,nsmall=0, digits=4),sub),
+     adj=c(1.2,0.7), cex=.75)
+sub <- !sub
+text(x=subset(x,sub),
+     y=subset(y,sub),
+     labels = subset(format(y,nsmall=0, digits=4),sub),
+     adj=c(-0.2,0.1), cex=.75)
+par(cex.axis=0.8)
+## draw the x axis
+axis(1,at=x)
+## draw the y axis
+y_at = seq(from = floor(min(y)/500)*500, 
+           to = ceiling(max(y)/500)*500, by=500)
+axis(2, at=y_at, labels=y_at)
+dev.off()
